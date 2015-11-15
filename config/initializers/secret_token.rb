71f534c0b30b2bb31e81fe6d0feb16fc24137248
uma senthil <uma_senthil@hotmail.com>
2015-11-15 02:10:52 +0000

    Create git repo for flixter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flixter::Application.config.secret_key_base = 'bd795d40dbbf607271309bea748ab97a3abc0076f060a9b112aa2d5d0c73957e8be196db3d8ae8f4679a50a160bdcf29c8ca6594005cb1359b140d582c9deef5'
