e9fea704cd6ef02683ce7d3e7bb6712aa614b4e5
uma senthil <uma_senthil@hotmail.com>
2015-11-18 20:18:21 +0000

    Added simple-form, defined actions in courses controller, got routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Flixter::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'static_pages#index'
+
+  namespace :instructor do
+    resources :courses, :only => [:new, :create, :show]
+  end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
