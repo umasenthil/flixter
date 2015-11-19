2f6b3ad7ef821e5825e0be1e264aa3143aff0608
uma senthil <uma_senthil@hotmail.com>
2015-11-19 04:23:09 +0000

    Added show and index page for student courses

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Flixter::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
   root 'static_pages#index'
 
+  resources :courses, :only => [:index, :show]
+
   namespace :instructor do
     resources :courses, :only => [:new, :create, :show]
   end
