16de3837ec18335c822802119487cfb5ea55f85d
umasenthil <uma_senthil@hotmail.com>
2015-12-03 18:17:32 +0000

    Add enrollment support for students

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,15 @@
 class LessonsController < ApplicationController
+	before_action :require_enrollment_for_viewing_lessons
 	def show
 	end
 
 	private
+	
+	def require_enrollment_for_viewing_lessons
+		if !current_user.enrolled_in?(current_lesson.section.course)
+			redirect_to course_path(current_lesson.section.course), :alert => 'Please enroll'
+		end
+	end
 
 	helper_method :current_lesson
 	def current_lesson
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	# the below line does the same thing as this line. has_many through is like a macro for this enrolled_courses = enrollments.collect(&:course)
 	# If we have done enrollments do each |enrollment| enrolled_courses << enrollment.course we have make one call to get all the enrollments 
 	# and make n calls to get course. By doing either of the above we are reducing N calls to database
-	has_many :enrolled_courses, :through => enrollments, :source => :course
+	has_many :enrolled_courses, :through => :enrollments, :source => :course
 	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
