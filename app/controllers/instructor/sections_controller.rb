803bc73f2fcf1016cda51bfa233b7c0513d54c9d
umasenthil <uma_senthil@hotmail.com>
2015-12-06 20:25:48 +0000

    Initial prep work for sections reordering

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
 	before_action :authenticate_user!
-	before_action :require_authorized_user_for_current_course
+	before_action :require_authorized_user_for_current_course, :only => [:new, :create]
+	before_action :require_authorized_user_for_current_section, :only => [:update]
 
 	def new
 		@section = Section.new
@@ -10,21 +11,31 @@ class Instructor::SectionsController < ApplicationController
 		@section = current_course.sections.create(section_params)
 		redirect_to instructor_course_path(current_course)
 	end
+	
+	def update
+	end
 
 	private
+	
+	def require_authorized_user_for_current_course
+		if current_course.user != current_user
+			render :text => "Unauthorized", :status => :Unauthorized
+		end
+	end
+	
+	def require_authorized_user_for_current_section
+		if current_course.section.user != current_user
+			render :text => "Unauthorized", :status => :Unauthorized
+		end
+	end
+
 
 	helper_method :current_course
 	def current_course
 		@current_course ||= Course.find(params[:course_id])
 	end
 
-	def require_authorized_user_for_current_course
-		if current_course.user != current_user
-			render :text => "Unauthorized", :status => :Unauthorized
-		end
-	end
-
 	def section_params
-		params.require(:section).permit(:title)
+		params.require(:section).permit(:title, :row_order_position)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Flixter::Application.routes.draw do
 
   namespace :instructor do
     resources :lessons, :only => [:update]
+    resources :sections, :only => [:update]
     
     resources :sections, :only => [] do
         resources :lessons, :only => [:new, :create]
