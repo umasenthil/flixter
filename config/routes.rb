3e974d397327f6632323395f73a5a6285b4d7634
umasenthil <uma_senthil@hotmail.com>
2015-12-07 02:45:35 +0000

    Added a modal for adding a section

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Flixter::Application.routes.draw do
         resources :lessons, :only => [:new, :create]
       end
     resources :courses, :only => [:new, :create, :show] do
-      resources :sections, :only => [:new, :create] 
+      resources :sections, :only => [:create] 
     end
   end
   # You can have the root of your site routed with "root"
