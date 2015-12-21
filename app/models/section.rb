f78a0b6e4868e1a11c1cbb42c9fada01efa766d5
umasenthil <uma_senthil@hotmail.com>
2015-12-21 02:39:32 +0000

    Cleanup the pages

--- app/models/section.rb
+++ app/models/section.rb
@@ -6,7 +6,7 @@ class Section < ActiveRecord::Base
 	ranks :row_order, :with_same => :course_id
 	
 	def next_section
-		section = course.section.where("row_order > ?", self.row_order).rank(:row_order).first
+		section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
 		return section
 	end
 end
